9597ee84f5892f4c3773b74912a4bfab3212ce6f
DenLeon <leonden0046@gmail.com>
2022-12-11 14:18:25 +0200

    Laba7 with sorting

--- Laba7/Laba7.cpp
+++ Laba7/Laba7.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <string>
+#include <algorithm>
 #include "Laba7.h"
 using namespace std;
 
@@ -15,15 +16,27 @@ const int shopSize = 2;
 void EnterProducts(Product  products[shopSize]);
 void FindProduct(string productName, Product  products[shopSize]);
 void OutputProduct(Product product);
+void OutputAllProducts(Product product[shopSize]);
+bool compare(Product a, Product b);
+void drawLine(int = 40, char = '=');
 
 int main()
 {
     Product products[shopSize];
     EnterProducts(products);
+    drawLine();
+    cout << "Before sorting: " << endl;
+    OutputAllProducts(products);
+    drawLine();
+    cout << "After sorting: " << endl;
+    sort(products, products + shopSize, compare);
+    OutputAllProducts(products);
+    drawLine();
     cout << "Find a product: " << endl;
     string prodName;
     cin >> prodName;
     FindProduct(prodName, products);
+
     return 0;
 }
 
@@ -36,6 +49,19 @@ void EnterProducts(Product  products[shopSize])
         cout << "ShopName: "; cin >> products[i].ShopName;
         cout << "Price: "; cin >> products[i].Price;
         //cout << "Enter name of the product" << "\t"; getline(cin, products[i].ProductName); cout << endl;
+    }
+}
+
+void OutputAllProducts(Product products[shopSize])
+{
+    cout << "Here is a list of all products" << endl;
+    for (int i = 0; i < shopSize; i++)
+    {
+        cout << "Product # " << (i + 1) << endl;
+        cout << " ProductName: "; cout << products[i].ProductName;
+        cout << " ShopName: "; cout << products[i].ShopName;
+        cout << " Price: "; cout << products[i].Price;
+        cout << endl;
     }
 }
 
@@ -58,3 +84,33 @@ void OutputProduct(Product product)
     cout << endl;
 }
 
+bool compare(Product a, Product b) {
+    if (a.ProductName.size() < b.ProductName.size())
+    {
+        return 1;
+    }
+    else if(a.ProductName.size() > b.ProductName.size())
+    {
+        return 0;
+    }
+    else
+    {
+        if (a.ShopName.size() < b.ShopName.size())
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
+
+void drawLine(int n, char c)
+{
+    cout.fill(c);
+    cout.width(n + 1);
+    cout << "\n";
+    cout.fill(' ');
+    //cout << setfill(c) << setw(n + 1) << '\n' << setfill(' ');
+}
